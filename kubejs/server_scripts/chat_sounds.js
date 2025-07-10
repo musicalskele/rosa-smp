23052d9dd32ec05b76d03e24fa4b304cf7d59c0d
musicalskele <musicalskele@gmail.com>
2025-07-10 15:19:01 +0300

    big changes :3

---
+++ kubejs/server_scripts/chat_sounds.js
@@ -0,0 +1,55 @@
+PlayerEvents.chat((event) => {
+
+	if (event.message.startsWith("/"))
+		return;
+
+	const words = event.message.toLowerCase().match(/[a-z]+/g) ?? [];
+	if (words.length === 0)
+		return;
+
+	let index = 0;
+	let speed = 0.5;
+
+	loopOverTime(() => {
+		if (index === words.length)
+			return -1;
+
+		const current = words[index];
+		const sound = getSoundIdFromKey(current);
+
+		index++;
+		if (sound === '') {
+			speed = Math.min(0.5, speed * 1.13);
+			return speed * 0.6 * SECOND;
+		}
+
+		const pitch = (Math.random() * 0.4 + 0.8).toFixed(2); // 0.8–1.2
+		Utils.server.runCommandSilent(`execute at @a run playsound ${sound} voice @a ~ ~ ~ 1 ${pitch}`);
+		speed *= 0.96;
+		return speed * SECOND;
+	});
+});
+
+function loopOverTime(ctx) {
+	const time = ctx();
+	if (time < 0)
+		return;
+	Utils.server.schedule(time, () => {
+		loopOverTime(ctx);
+	});
+}
+
+function getSoundIdFromKey(key) {
+	switch (key) {
+		case 'meow': case 'mnya': case 'mnyaa': case 'nyaa': case 'mreow':
+			return 'entity.cat.ambient';
+		case 'mrow':
+			return 'entity.cat.purreow';
+		case 'bark' : case 'woof':
+			return 'entity.wolf.ambient';
+		case 'baa':
+			return 'entity.sheep.ambient';
+		default:
+			return '';
+	}
+}
